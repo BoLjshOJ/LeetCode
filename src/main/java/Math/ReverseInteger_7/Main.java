9a6c6f92f39c761f7a64599524389da18195eb96
BoLjshOJ <boljshoj@gmial.com>
2018-06-05 16:06:10 +0300

    resolved task Reverse Integer #3

---
+++ src/main/java/Math/ReverseInteger_7/Main.java
@@ -0,0 +1,11 @@
+package Math.ReverseInteger_7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int result = s.reverse(-141234578);
+        System.out.println(result);
+    }
+
+}
